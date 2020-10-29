4baff3ec25653ff4f4c7238dd91e07df56ce986f
eugenmorok <64523467+eugenmorok@users.noreply.github.com>
2020-10-29 16:46:48 +0700

    29-10-2020-1

--- src/EqualDigitsOf3.java
+++ src/EqualDigitsOf3.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class EqualDigitsOf3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter three digits number: ");
+        int inNum = sc.nextInt();
+        int u1 = inNum / 100;
+        int u2 = inNum % 100 / 10;
+        int u3 = inNum % 10;
+
+        if (u1 == u2 | u1 == u3 | u2 == u3) System.out.println("our number has the equal digits");
+        else System.out.println(("our number has't the equal digits"));
+        System.out.println("the end");
+    }
 }
